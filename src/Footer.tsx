b049a7819ea108fcf475a2a95e1236777a5071c3
2_ØØØØØØ1 <61893884+developermodoki@users.noreply.github.com>
2022-05-08 14:17:12 +0900

    Update Footer.tsx

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = ():JSX.Element => {
     return (
         <div className="footer">
             <div className="footerCopyright">©うぃん</div>
-            <div className="privacy"><a href="/">Privacy</a></div>
+            <div className="privacy"><a href="https://github.com/developermodoki/win-public/blob/main/README.md#privacy">Privacy</a></div>
         </div>
     );
 }
